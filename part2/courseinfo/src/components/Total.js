ad158eed3f8fe4d351b1a0a864c0e340c4ba0be6
jerrytohvan <jerrytohvan@gmail.com>
2023-03-27 21:39:34 +0800

    2.3*: Course information step8 submission

--- part2/courseinfo/src/components/Total.js
+++ part2/courseinfo/src/components/Total.js
@@ -1,8 +1,8 @@
 export const Total = (props) => {
-    const totalExercise = props.parts.reduce((accumulator, content) => accumulator + content.exercises, 0)
+    const totalExercises = props.parts.reduce((accumulator, content) => accumulator + content.exercises, 0)
     return (
       <b>
-        Total of {totalExercise} exercises
+        Total of {totalExercises} exercises
       </b>
     )
   };
