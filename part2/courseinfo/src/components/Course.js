70b1563f6e6b18676494be6fca96f4764428cb7e
jerrytohvan <jerrytohvan@gmail.com>
2023-03-27 21:33:36 +0800

    2.2: Course information step7 submission

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -1,12 +1,13 @@
 import { Header } from './Header';
 import { Content } from './Content';
+import { Total } from './Total';
 
 export const Course = ({ course }) => {
     return (
         <>
             <Header course={course.name} />
             <Content parts={course.parts} />
-            {/* <Total parts={course.parts} /> */}
+            <Total parts={course.parts} />
         </>
     )
 };
--- part2/courseinfo/src/components/Total.js
+++ part2/courseinfo/src/components/Total.js
@@ -1,9 +1,9 @@
 export const Total = (props) => {
     const totalExercise = props.parts.reduce((accumulator, content) => accumulator + content.exercises, 0)
     return (
-      <p>
-        Number of exercises {totalExercise}
-      </p>
+      <b>
+        Total of {totalExercise} exercises
+      </b>
     )
   };
   
