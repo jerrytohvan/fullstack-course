4fb59568863123cc8514cb8a7c39e0c907080b7c
jerrytohvan <jerrytohvan@gmail.com>
2023-04-01 17:13:22 +0800

    part 2.3 add notes filtering

--- part2-2/src/App.js
+++ part2-2/src/App.js
@@ -6,6 +6,7 @@ const App = (props) => {
   const [newNote, setNewNote] = useState(
     'a new note...'
   );
+  const [showAll, setShowAll] = useState(true)
 
   const addNote = (event) => {
     event.preventDefault()
@@ -14,7 +15,7 @@ const App = (props) => {
       important: Math.random() < 0.5,
       id: notes.length + 1,
     }
-  
+
     setNotes(notes.concat(noteObject))
     setNewNote('')
   }
@@ -23,12 +24,21 @@ const App = (props) => {
     setNewNote(event.target.value)
   };
 
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important);
+
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
       <ul>
         <ul>
-          {notes.map(note =>
+          {notesToShow.map(note =>
             <Note key={note.id} note={note} />
           )}
         </ul>
