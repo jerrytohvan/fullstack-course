d4940e9c448985a87e52262613359326686e99c0
jerrytohvan <jerrytohvan@gmail.com>
2023-04-01 18:17:15 +0800

    2.9*: The Phonebook Step4 submission

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,10 +3,10 @@ import { v1 as uuidv1 } from 'uuid';
 
 const App = () => {
   const [persons, setPersons] = useState([
-    {
-      id: uuidv1(),
-      name: 'Arto Hellas'
-    }
+    { name: 'Arto Hellas', number: '040-123456', id: uuidv1() },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: uuidv1() },
+    { name: 'Dan Abramov', number: '12-43-234345', id: uuidv1() },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: uuidv1() }
   ]);
 
   const [newPhonebook, setNewPhonebook] = useState({
@@ -14,6 +14,7 @@ const App = () => {
     number: ''
   });
 
+  const [filterName, setFilterName] = useState('');
 
   const isPhonebookExists = () => {
     const existingPerson = persons.filter(person => person.name === newPhonebook.name);
@@ -26,6 +27,9 @@ const App = () => {
     }
     if (event.target.id === 'number') {
       setNewPhonebook({ ...newPhonebook, number: event.target.value });
+    }
+    if (event.target.id === 'filter') {
+      setFilterName(event.target.value);
     }
   };
 
@@ -48,10 +52,18 @@ const App = () => {
     });
   };
 
+  const getAllNames = () => {
+    if (filterName === '') return persons;
+    return persons.filter(person => person.name.toLocaleLowerCase().includes(filterName.toLocaleLowerCase()));
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with a: <input type="text" id='filter' onChange={handleInputChange} />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={handleSubmit}>
         <div>
           name: <input type="text" id='name' onChange={handleInputChange} />
@@ -59,18 +71,13 @@ const App = () => {
         <div>
           number: <input type="text" id='number' onChange={handleInputChange} />
         </div>
-        <>
-          debug: {
-            `${newPhonebook.name} ${newPhonebook.number}`
-          }
-        </>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       {
-        persons.map(person => <div key={person.id}><p>{person.name}</p></div>)
+        getAllNames().map(person => <div key={person.id}><p>{person.name} {person.number}</p></div>)
       }
     </div>
   );
