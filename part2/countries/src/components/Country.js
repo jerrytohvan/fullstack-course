80c777be7b2db09b8bee1f3275f55e4af8863c91
jerrytohvan <jerrytohvan@gmail.com>
2023-04-07 17:55:52 +0800

    2.20*: Data for countries, step3 submission

--- part2/countries/src/components/Country.js
+++ part2/countries/src/components/Country.js
@@ -1,28 +1,58 @@
+import { useState, useEffect } from 'react'
+import { getWeather } from '../services/weather'
 
 const getLanguages = (languages) => {
   const keys = Object.keys(languages);
   const mappedLanguages = keys.map(key => languages[key]);
   return mappedLanguages;
-} 
+}
 
 export const Country = ({ country }) => {
+  const [weather, setWeather] = useState(null)
+
+  const capitalCity = country.capital[0] ?? null;
+
+  useEffect(() => {
+    getWeather(capitalCity).then(
+      data => setWeather(data)
+    ).catch(
+      error => {
+        console.error(`Something wrong with fetching ${capitalCity} weather`, error.message)
+      }
+    )
+  }, []);
+
   return (
     <>
       <h1>
         {country.name.official}
       </h1>
-      <br/>
-      <p>capital {country.capital[0] ?? 'None'}</p>
+      <br />
+      <p>capital {capitalCity ?? 'None'}</p>
       <p>area {country.area}</p>
-      <br/>
+      <br />
       <p><b>languages:</b></p>
       <ul>
         {
-          getLanguages(country.languages).map(language => <li>{language}</li>)
+          getLanguages(country.languages).map(language => <li key={language}>{language}</li>)
         }
       </ul>
-      <br/>
+      <br />
       <img src={country.flags['png']} alt={country.alt} />
+      <br />
+      {
+        weather &&
+        (
+          <>
+            <h1>Weather in {weather.name}</h1>
+            <>
+              <p>temperature {weather.main.temp} Celcius</p>
+              <img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.name} />
+              <p>wind {weather.wind.speed} m/s</p>
+            </>
+          </>
+        )
+      }
     </>
   )
 }
--- part2/countries/src/components/FilteredCountries.js
+++ part2/countries/src/components/FilteredCountries.js
@@ -5,10 +5,10 @@ export const FilteredCountries = ({ countries, setCountry }) => {
   return (
     countries.map(country =>
     (
-      <>
+      <div key={country.name.official}>
         <p>{country.name.official}
           <button onClick={() => setCountry(country)}>show</button></p>
-      </>
+      </div>
     )
     )
   )
