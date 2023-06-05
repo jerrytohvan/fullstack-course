b5e1a3a44942b1c6d70d8189208b7f666d02f180
Jerry Tohvan <jerrytohvan@gmail.com>
2023-06-05 22:16:20 +0800

    Add course material

---
+++ part3-1/notes-backend/mongo.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://jerrytohvan:${password}@fullstack-course.o2kvfuo.mongodb.net/?retryWrites=true&w=majority`;
+mongoose.set("strictQuery", false);
+mongoose.connect(url);
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+});
+
+const Note = mongoose.model("Note", noteSchema);
+
+note.save().then((result) => {
+  console.log("note saved!");
+  mongoose.connection.close();
+});
+
+Note.find({ important: true }).then((result) => {
+  result.forEach((note) => {
+    console.log(note);
+  });
+  mongoose.connection.close();
+});
