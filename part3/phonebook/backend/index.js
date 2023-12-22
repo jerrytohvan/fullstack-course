fc7df3fbeb45c402d4247cc6e4d1afa5b298b3a7
Jerry Tohvan <jerrytohvan@gmail.com>
2023-12-22 16:26:35 +0800

    Remove unused static data

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -22,29 +22,6 @@ const { createDbConnection } = require("./models/client.js");
 const { Phonebook } = require("./models/phonebook.js");
 const { default: mongoose } = require("mongoose");
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
 const getAllPersons = async () => {
   await createDbConnection();
   return Phonebook.find({})
