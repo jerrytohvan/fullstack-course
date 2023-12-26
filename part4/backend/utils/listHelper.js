6aaac26200c3c2d7cdbf4e32a656f866c4f2ce4c
Jerry Tohvan <jerrytohvan@gmail.com>
2023-12-26 16:21:12 +0800

    4.3: helper functions and unit tests, step1

---
+++ part4/backend/utils/listHelper.js
@@ -0,0 +1,12 @@
+// https://fullstackopen.com/osa4/sovelluksen_rakenne_ja_testauksen_alkeet#tehtavat-4-3-4-7
+
+const dummy = (blogs) => {
+  if (blogs) {
+    return 1;
+  }
+  return 1;
+};
+
+module.exports = {
+  dummy,
+};
