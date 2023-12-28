02782ec92a2673f0cbb2fc00ddd5b69c99fd2e69
Jerry Tohvan <jerrytohvan@gmail.com>
2023-12-28 12:25:30 +0800

    Part 4d practice

--- part4-1/notes-backend/app.js
+++ part4-1/notes-backend/app.js
@@ -5,8 +5,11 @@ require('express-async-errors');
 
 const app = express();
 const cors = require('cors');
+
 const notesRouter = require('./controllers/notes');
 const usersRouter = require('./controllers/users');
+const loginRouter = require('./controllers/login');
+
 const middleware = require('./utils/middleware');
 const config = require('./utils/config');
 const logger = require('./utils/logger');
@@ -30,6 +33,7 @@ app.use(express.json());
 app.use(express.static('build'));
 app.use(middleware.requestLogger);
 
+app.use('/api/login', loginRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/notes', notesRouter);
 
