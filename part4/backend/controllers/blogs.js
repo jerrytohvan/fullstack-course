e1b44e5c0a03aed8ddc76d162261b0d41dc7cb3e
Jerry Tohvan <jerrytohvan@gmail.com>
2023-12-28 13:33:25 +0800

    4.17: bloglist expansion, step5

--- part4/backend/controllers/blogs.js
+++ part4/backend/controllers/blogs.js
@@ -1,9 +1,10 @@
 const blogsRouter = require('express').Router();
 
 const Blog = require('../models/blog');
+const User = require('../models/user');
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await  Blog.find({});
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 });
 
   response.json(blogs);
 });
@@ -11,7 +12,7 @@ blogsRouter.get('/', async (request, response) => {
 blogsRouter.get('/:id', async (request, response) => {
   const blogs = await Blog.findById(request.params.id);
 
-  if(blogs){
+  if (blogs) {
     response.json(blogs);
   }
   response.status(404).end();
@@ -19,14 +20,21 @@ blogsRouter.get('/:id', async (request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body;
+
+  const user = await User.findById(body.userId);
+
   const newBlog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
     likes: body.likes || 0,
+    user: user.id,
   });
-  await newBlog
-    .save();
+  await newBlog.save();
+
+  user.blogs = user.blogs.concat(newBlog.id);
+  await user.save();
+
   response.status(201).json(newBlog);
 });
 
@@ -40,7 +48,9 @@ blogsRouter.put('/:id', async (request, response) => {
   const blog = {
     likes: body.likes || 0,
   };
-  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true });
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {
+    new: true,
+  });
   response.json(updatedBlog);
 });
 
--- part4/backend/controllers/users.js
+++ part4/backend/controllers/users.js
@@ -22,7 +22,7 @@ userRouter.post('/', async (request, response) => {
 });
 
 userRouter.get('/', async (request, response) => {
-  const users = await User.find({});
+  const users = await User.find({}).populate('blogs', { url: 1, title: 1, author: 1 });
   response.status(200).json(users);
 });
 
