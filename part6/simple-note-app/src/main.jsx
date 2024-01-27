75c9a28bdbd99f4554d640ca6814213ff0fbd271
Jerry Tohvan <jerrytohvan@gmail.com>
2024-01-27 21:13:42 +0800

    Part6-3 practice

--- part6/simple-note-app/src/main.jsx
+++ part6/simple-note-app/src/main.jsx
@@ -1,18 +1,16 @@
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { configureStore, combineReducers } from '@reduxjs/toolkit'
-import { noteReducer, createNote } from './reducers/noteReducer'
+import { configureStore } from '@reduxjs/toolkit'
+import noteReducer, { createNote } from './reducers/noteReducer'
 import { filterChange, filterReducer } from './reducers/filterReducer'
 import { Provider } from 'react-redux'
 
 import App from './App.jsx'
 
-const reducer = combineReducers({
+const store = configureStore({ reducer: {
   notes: noteReducer,
   filter: filterReducer
-})
-
-const store = configureStore({ reducer })
+} })
 
 
 store.subscribe(() => console.log(store.getState()))
@@ -24,3 +22,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </Provider>
 )
+
+
+// https://fullstackopen.com/en/part6/many_reducers#redux-toolkit
--- part6/simple-note-app/src/reducers/noteReducer.test.js
+++ part6/simple-note-app/src/reducers/noteReducer.test.js
@@ -1,4 +1,4 @@
-import { noteReducer } from './noteReducer'
+import noteReducer from './noteReducer'
 import deepFreeze from 'deep-freeze'
 
 // https://fullstackopen.com/en/part6/flux_architecture_and_redux/
@@ -6,19 +6,15 @@ describe('noteReducer', () => {
   test('returns new state with action NEW_NOTE', () => {
     const state = []
     const action = {
-      type: 'NEW_NOTE',
-      payload: {
-        content: 'the app state is in redux store',
-        important: true,
-        id: 1,
-      },
+      type: 'notes/createNote',
+      payload: 'the app state is in redux store',
     }
 
     deepFreeze(state)
     const newState = noteReducer(state, action)
 
     expect(newState).toHaveLength(1)
-    expect(newState).toContainEqual(action.payload)
+    expect(newState[0].content).toEqual(action.payload)
   })
 
   test('returns new state with action TOGGLE_IMPORTANCE', () => {
@@ -36,10 +32,8 @@ describe('noteReducer', () => {
     ]
 
     const action = {
-      type: 'TOGGLE_IMPORTANCE',
-      payload: {
-        id: 2,
-      },
+      type: 'notes/toggleImportanceOf',
+      payload: 2
     }
 
     deepFreeze(state)
