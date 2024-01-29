cb3e95784c6583cfe8b8cc90b3d9e72ee9efb6d3
Jerry Tohvan <jerrytohvan@gmail.com>
2024-01-29 22:15:07 +0800

    Exercise 6.20

--- part6/query-anecdotes/src/App.jsx
+++ part6/query-anecdotes/src/App.jsx
@@ -1,40 +1,59 @@
-import AnecdoteForm from './components/AnecdoteForm'
-import Notification from './components/Notification'
+import AnecdoteForm from "./components/AnecdoteForm";
+import Notification from "./components/Notification";
+import { useQueryClient, useQuery } from "@tanstack/react-query";
+import { getAnecdotes } from "./requests";
 
+//https://fullstackopen.com/en/part6/react_query_use_reducer_and_the_context#exercises-6-20-6-22
 const App = () => {
+  // const queryClient = useQueryClient();
 
-  const handleVote = (anecdote) => {
-    console.log('vote')
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ["anecdotes"],
+    queryFn: getAnecdotes,
+    refetchOnWindowFocus: false,
+    retry: 1,
+  });
+
+  if (isLoading) {
+    return <div>loading data...</div>;
   }
 
-  const anecdotes = [
-    {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    },
-  ]
+  if (isError) {
+    return (
+      <div>
+        {" "}
+        <p>
+          anecdote server is not available due to problems in server:{" "}
+          {error.message}
+        </p>
+      </div>
+    );
+  }
+
+  const handleVote = (anecdote) => {
+    console.log("vote");
+  };
+
+  const anecdotes = data;
 
   return (
     <div>
       <h3>Anecdote app</h3>
-    
+
       <Notification />
       <AnecdoteForm />
-    
-      {anecdotes.map(anecdote =>
+
+      {anecdotes.map((anecdote) => (
         <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
+          <div>{anecdote.content}</div>
           <div>
             has {anecdote.votes}
             <button onClick={() => handleVote(anecdote)}>vote</button>
           </div>
         </div>
-      )}
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
